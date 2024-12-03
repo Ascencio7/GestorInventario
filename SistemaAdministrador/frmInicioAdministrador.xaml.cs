a9db96502932077fb3e3e7145f1643de92dc8485
Vladimir Ascencio <vladimirramos405@gmail.com>
2024-12-03 15:31:07 -0600

    Unos cambios sin importancia

--- SistemaAdministrador/frmInicioAdministrador.xaml.cs
+++ SistemaAdministrador/frmInicioAdministrador.xaml.cs
@@ -29,26 +29,8 @@ namespace GestorInventario.SistemaAdministrador
             InitializeComponent();
         }
 
-        private void frPrincipal_Loaded(object sender, RoutedEventArgs e)
-        {
-            frPrincipal.NavigationService.Navigate(new Uri("SistemaAdministrador/pageBienvenidoAdmin.xaml", UriKind.Relative));
 
-        }
-
-        private void btnGestionRegistroAdmin_Click(object sender, RoutedEventArgs e)
-        {
-            frmRegistro formRegistro = new frmRegistro();
-            this.Hide();
-            formRegistro.Show();
-        }
-
-        private void btnGestionUsuariosInactivosAdmin_Click(object sender, RoutedEventArgs e)
-        {
-            UsuariosInactivosAdmin formUsuariosInactivos = new UsuariosInactivosAdmin();
-            this.Hide();
-            formUsuariosInactivos.Show();
-        }
-
+        #region Colores Botones
         private void btnGestionRegistroAdmin_MouseEnter(object sender, MouseEventArgs e)
         {
             btnGestionRegistroAdmin.Background = new SolidColorBrush(Color.FromRgb(41, 16, 153));
@@ -69,17 +51,6 @@ namespace GestorInventario.SistemaAdministrador
             btnGestionUsuariosInactivosAdmin.Background = new SolidColorBrush(Color.FromRgb(5, 135, 137)); // Color original
         }
 
-        private void btnSalirAdmin_Click(object sender, RoutedEventArgs e)
-        {
-            if(MessageBox.Show("¿Desea salir del Sistema Administrador?", "ATLAS CORP | SISTEMA ADMINISTRADOR", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
-            {
-                MessageBox.Show("Redireccionando al Inicio de Sesión", "ATLAS CORP", MessageBoxButton.OK,MessageBoxImage.Information);
-                Login formLogin = new Login();
-                this.Hide();
-                formLogin.Show();
-            }
-        }
-
         private void btnSalirAdmin_MouseEnter(object sender, MouseEventArgs e)
         {
             btnSalirAdmin.Background = new SolidColorBrush(Color.FromRgb(41, 16, 153));
@@ -88,15 +59,6 @@ namespace GestorInventario.SistemaAdministrador
         private void btnSalirAdmin_MouseLeave(object sender, MouseEventArgs e)
         {
             btnSalirAdmin.Background = new SolidColorBrush(Color.FromRgb(5, 135, 137)); // Color original
-        }
-
-        private void btnCerrarAplicacionAdmin_Click(object sender, RoutedEventArgs e)
-        {
-            if(MessageBox.Show("¿Desea cerrar la aplicación?", "ATLAS CORP", MessageBoxButton.YesNo, MessageBoxImage.Question)== MessageBoxResult.Yes)
-            {
-                MessageBox.Show("Cerrando la aplicación desde el Sistema Administrador.", "ATLAS CORP | CERRANDO APLICACIÓN", MessageBoxButton.OK, MessageBoxImage.Information);
-                Application.Current.Shutdown();
-            }
         }
 
         private void btnCerrarAplicacionAdmin_MouseEnter(object sender, MouseEventArgs e)
@@ -109,13 +71,6 @@ namespace GestorInventario.SistemaAdministrador
             btnCerrarAplicacionAdmin.Background = new SolidColorBrush(Color.FromRgb(5, 135, 137)); // Color original
         }
 
-        private void btnGestionProductosAdmin_Click(object sender, RoutedEventArgs e)
-        {
-            ProductosAdmin formProductosAdmin = new ProductosAdmin();
-            this.Hide();
-            formProductosAdmin.Show();
-        }
-
         private void btnGestionProductosAdmin_MouseEnter(object sender, MouseEventArgs e)
         {
             btnGestionProductosAdmin.Background = new SolidColorBrush(Color.FromRgb(41, 16, 153));
@@ -124,13 +79,6 @@ namespace GestorInventario.SistemaAdministrador
         private void btnGestionProductosAdmin_MouseLeave(object sender, MouseEventArgs e)
         {
             btnGestionProductosAdmin.Background = new SolidColorBrush(Color.FromRgb(5, 135, 137)); // Color original
-        }
-
-        private void btnReportesAdmin_Click(object sender, RoutedEventArgs e)
-        {
-            menuReportes formMenuReportes = new menuReportes();
-            this.Hide();
-            formMenuReportes.Show();
         }
 
         private void btnReportesAdmin_MouseEnter(object sender, MouseEventArgs e)
@@ -142,5 +90,90 @@ namespace GestorInventario.SistemaAdministrador
         {
             btnReportesAdmin.Background = new SolidColorBrush(Color.FromRgb(5, 135, 137)); // Color original
         }
+        #endregion
+
+
+
+        #region Page de bienvenida
+        private void frPrincipal_Loaded(object sender, RoutedEventArgs e)
+        {
+            frPrincipal.NavigationService.Navigate(new Uri("SistemaAdministrador/pageBienvenidoAdmin.xaml", UriKind.Relative));
+        }
+        #endregion
+
+
+
+        #region Boton de Registrar Usuarios
+        private void btnGestionRegistroAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            frmRegistro formRegistro = new frmRegistro();
+            this.Hide();
+            formRegistro.Show();
+        }
+        #endregion
+
+        
+
+        #region Boton Gestion Usuario Inactivo
+        private void btnGestionUsuariosInactivosAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            UsuariosInactivosAdmin formUsuariosInactivos = new UsuariosInactivosAdmin();
+            this.Hide();
+            formUsuariosInactivos.Show();
+        }
+        #endregion
+
+
+
+        #region Boton Gestion Productos
+        private void btnGestionProductosAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            ProductosAdmin formProductosAdmin = new ProductosAdmin();
+            this.Hide();
+            formProductosAdmin.Show();
+        }
+        #endregion
+
+
+
+        #region Boton Reportes
+        private void btnReportesAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            menuReportes formMenuReportes = new menuReportes();
+            this.Hide();
+            formMenuReportes.Show();
+        }
+        #endregion
+
+
+
+        #region Boton Salir al Login
+        private void btnSalirAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("¿Desea salir del Sistema Administrador?", "ATLAS CORP | SISTEMA ADMINISTRADOR", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Redireccionando al Inicio de Sesión", "ATLAS CORP", MessageBoxButton.OK,MessageBoxImage.Information);
+                Login formLogin = new Login();
+                this.Hide();
+                formLogin.Show();
+            }
+        }
+        #endregion
+
+
+
+        #region Boton Cerrar la Aplicacion
+        private void btnCerrarAplicacionAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("¿Desea cerrar la aplicación?", "ATLAS CORP", MessageBoxButton.YesNo, MessageBoxImage.Question)== MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Cerrando la aplicación desde el Sistema Administrador.", "ATLAS CORP | CERRANDO APLICACIÓN", MessageBoxButton.OK, MessageBoxImage.Information);
+                Application.Current.Shutdown();
+            }
+        }
+        #endregion
+
+
+
     }
 }
